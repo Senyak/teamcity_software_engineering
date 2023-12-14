33cd9cba596919d93816dc4ea30c6d4ad74ececa
senyak <mselina@sfedu.ru>
2023-12-14 13:33:10 +0300

    add project

---
+++ GCD_for_sailors/GCD_for_sailors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCD_for_sailors
+{
+    internal class Program
+    {
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                var t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Алгоритм Евклида");
+            Console.WriteLine("Наибольший общий делитель чисел {0} и {1} равен {2}", 294, 162, GCD(294, 162));
+            Console.ReadLine();
+        }
+    }
+}
